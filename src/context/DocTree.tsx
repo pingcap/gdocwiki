755651aa7f3c11aa7bcd9862066a55fdfa7e72f8
hexilee <i@hexilee.me>
2020-12-01 17:25:37 +0800

    format DocTree.tsx
    Signed-off-by: hexilee <i@hexilee.me>

--- src/context/DocTree.tsx
+++ src/context/DocTree.tsx
@@ -71,7 +71,7 @@ export function DocTreeProvider({ children }) {
         data: itemsByParent[RootDriveId] ?? [],
         dataFlat: allItems,
       }));
-    } 
+    }
     catch (e) {
       console.log(e)
     } finally {
