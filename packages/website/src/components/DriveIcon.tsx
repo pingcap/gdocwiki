2d154cf35efbdce6d769b016868509994850b32a
Greg Weber <greg@pingcap.com>
2021-07-22 21:07:39 -0500

    doc view: thumbnail preview of gdoc links

--- packages/website/src/components/DriveIcon.tsx
+++ packages/website/src/components/DriveIcon.tsx
@@ -1,5 +1,4 @@
 import { Launch16 } from '@carbon/icons-react';
-import React from 'react';
 import { DriveFile, mdLink, MimeTypes } from '../utils';
 
 export function DriveIcon({ file }: { file?: DriveFile }) {
@@ -22,6 +21,6 @@ export function DriveIcon({ file }: { file?: DriveFile }) {
   return <img src={src} width={16} alt="File Icon" />;
 }
 
-DriveIcon.getIconSrc = (mimeType, size = 32) => {
+DriveIcon.getIconSrc = (mimeType: string, size = 32) => {
   return `https://drive-thirdparty.googleusercontent.com/${size}/type/${mimeType}`;
 };
