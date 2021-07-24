0f1f3f74110d95fe5656630519c11cf2f2b2b1fa
Greg Weber <greg@pingcap.com>
2021-07-24 11:34:14 -0500

    header outline: show document title centered
    This looks better than the previous left-aligned text "Outline"

--- packages/website/src/reduxSlices/doc.ts
+++ packages/website/src/reduxSlices/doc.ts
@@ -1,12 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { DriveFile } from '../utils';
 import { DocHeader, TreeHeading } from '../utils/docHeaders';
 
 export interface DocState {
+  file: DriveFile | null;
   headers?: (TreeHeading | DocHeader)[];
   comments: gapi.client.drive.Comment[];
 }
 
 const initialStateDoc: DocState = {
+  file: null,
   headers: [],
   comments: [],
 };
@@ -15,6 +18,12 @@ export const slice = createSlice({
   name: 'doc',
   initialState: initialStateDoc,
   reducers: {
+    setFile: (state, { payload }: { payload: DriveFile }) => {
+      state.file = payload;
+    },
+    setNoFile: (state, { payload }: { payload: undefined }) => {
+      state.file = null;
+    },
     setHeaders: (state, { payload }: { payload: (TreeHeading | DocHeader)[] }) => {
       state.headers = payload;
     },
@@ -24,9 +33,10 @@ export const slice = createSlice({
   },
 })
 
-export const { setHeaders, setComments } = slice.actions;
+export const { setFile, setNoFile, setHeaders, setComments } = slice.actions;
 
 export const selectHeaders = (state: { doc: DocState }) => state.doc.headers;
 export const selectComments = (state: { doc: DocState }) => state.doc.comments;
+export const selectDriveFile = (state: { doc: DocState }) => state.doc.file;
 
 export default slice.reducer;
