5ff80ed1d22e02b5b7bada94ad714054a73bfd96
Greg Weber <greg@gregweber.info>
2021-08-05 08:16:51 -0500

    File name query param (#116)
    In the browser extension add the document name to the url parameters
    
    Then when the link is shared by copying from the browser url
    the title will be in the shared link.
    The viewer of the link will know what the document is about
    even if there is no link preview.
    
    Be more stringent with the query param characters.
    
    The browser will show invalid characters in the browser bar.
    However, when copying the characters they may be percent-encoded.

--- packages/browser-extension/src/contentScript/docs.tsx
+++ packages/browser-extension/src/contentScript/docs.tsx
@@ -47,6 +47,29 @@ async function loadFileInfo(fileId: string, token: Token): Promise<FileInfo | un
     fields: '*',
     supportsAllDrives: true,
   });
+
+  // Put the document name in the URL
+  // Then when we send the link to others they will see the title
+  try {
+    var urlParams = new URLSearchParams(window.location.search);
+    if (file.name && !urlParams.get('n')) {
+      // Change invalid characters to '_' but don't show 2 of those next to each other
+      // Change '_-_' to '-'
+      const paramValue = encodeURIComponent(file.name.replaceAll('%', '_'))
+        .replaceAll(/%../g, '_')
+        .replaceAll(/_+/g, '_')
+        .replaceAll(/_-/g, '-')
+        .replaceAll(/-_+/g, '-')
+        .replace(/_+$/, '')
+        .replace(/^_+/, '');
+      urlParams.set('n', paramValue);
+      const newUrl = window.location.origin + window.location.pathname + '?' + urlParams.toString();
+      window.history.replaceState({path: newUrl}, "", newUrl);
+    }
+  } catch (e) {
+    console.warn("error trying to add doc name to parameters ", e);
+  }
+
   log.info('File metadata', file);
   if (file.trashed) {
     log.info('Skipped the file since it is trashed');
@@ -296,7 +319,7 @@ function App(props: { id: string }) {
 }
 
 export async function runDocs(id: string) {
-  // Add ?versions to the url params to deep-link to the versions page
+  // Users can add ?versions to the url params to deep-link to the versions page
   var urlParams = new URLSearchParams(window.location.search);
   if (urlParams.has('versions')) {
     console.debug('wait to click on versions link');
